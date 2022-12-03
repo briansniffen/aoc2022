660125846bc806a4a99bbaa5c6b04b1c4f265b12
Brian Sniffen <bsniffen@fb.com>
2022-12-02 22:30:36 -0500

    this makes me happier

--- 2/rps/src/main.rs
+++ 2/rps/src/main.rs
@@ -1,4 +1,7 @@
 use std::io::{self, BufRead};
+extern crate num;
+#[macro_use]
+extern crate num_derive;
 
 // The winner of the whole tournament is the player with the highest
 // score. Your total score is the sum of your scores for each
@@ -14,18 +17,18 @@ use std::io::{self, BufRead};
 // draw 3
 // win  6
 
-#[derive(PartialEq)]
+#[derive(PartialEq, FromPrimitive)]
 enum RPS {
-    Rock,
-    Paper,
-    Scissor,
+    Rock = 1,
+    Paper = 2,
+    Scissor = 3,
 }
 
-#[derive(PartialEq)]
+#[derive(PartialEq, FromPrimitive)]
 enum Game {
-    Lose,
-    Draw,
-    Win,
+    Lose = 0,
+    Draw = 3,
+    Win = 6,
 }
 
 fn parse(c: Option<char>) -> RPS {
@@ -48,58 +51,29 @@ fn parse2(c: Option<char>) -> Game {
 
 // game state for player a
 fn game(a: RPS, b: RPS) -> Game {
-    return match (a, b) {
-        (x, y) if x == y => Game::Draw,
-        (RPS::Rock, RPS::Scissor) => Game::Win,
-        (RPS::Scissor, RPS::Paper) => Game::Win,
-        (RPS::Paper, RPS::Rock) => Game::Win,
+    return match a as i32 - b as i32 {
+        0 => Game::Draw,
+        -2 => Game::Win,
+        1 => Game::Win,
         _ => Game::Lose,
     };
 }
 
-fn win_vs(a: RPS) -> RPS {
-    return match a {
-        RPS::Rock => RPS::Paper,
-        RPS::Paper => RPS::Scissor,
-        RPS::Scissor => RPS::Rock,
-    };
-}
-
-fn lose_vs(a: RPS) -> RPS {
-    return match a {
-        RPS::Rock => RPS::Scissor,
-        RPS::Paper => RPS::Rock,
-        RPS::Scissor => RPS::Paper,
-    };
-}
-
-fn draw_vs(a: RPS) -> RPS {
-    return a;
+fn play_vs(need: Game, them: RPS) -> RPS {
+    return num::FromPrimitive::from_i32(
+        ((((them as i32 + ((need as i32 - 3) / 3) - 1) % 3) + 3) % 3) + 1,
+    )
+    .unwrap();
 }
 
 fn score(s: &str) -> i32 {
-    let score = match game(parse(s.chars().nth(2)), parse(s.chars().nth(0))) {
-        Game::Win => 6,
-        Game::Draw => 3,
-        Game::Lose => 0,
-    } + piece_score(parse(s.chars().nth(2)));
-    return score;
-}
-
-fn piece_score(r: RPS) -> i32 {
-    return match r {
-        RPS::Rock => 1,
-        RPS::Paper => 2,
-        RPS::Scissor => 3,
-    };
+    return game(parse(s.chars().nth(2)), parse(s.chars().nth(0))) as i32
+        + parse(s.chars().nth(2)) as i32;
 }
 
 fn score2(s: &str) -> i32 {
-    let score = match parse2(s.chars().nth(2)) {
-        Game::Win => 6 + piece_score(win_vs(parse(s.chars().nth(0)))),
-        Game::Draw => 3 + piece_score(draw_vs(parse(s.chars().nth(0)))),
-        Game::Lose => 0 + piece_score(lose_vs(parse(s.chars().nth(0)))),
-    };
+    let score = (parse2(s.chars().nth(2)) as i32)
+        + play_vs(parse2(s.chars().nth(2)), parse(s.chars().nth(0))) as i32;
     return score;
 }
 
