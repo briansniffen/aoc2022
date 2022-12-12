993e4c0a784f7049de9a6e4a07f1cd33d5f0c015
Brian Sniffen <bsniffen@akamai.com>
2022-12-12 15:06:51 -0500

    12: Clarity and reorganization

--- 12/height/src/main.rs
+++ 12/height/src/main.rs
@@ -26,10 +26,9 @@ fn dijkstra(map: &[Vec<u8>], start_x: usize, start_y: usize, up: bool) -> Vec<Ve
             if new_x >= rows || new_y >= cols {
                 continue;
             }
-            if (up && ((map[new_x][new_y] <= (1 + map[x][y])) && (dist[new_x][new_y] > cost + 1)))
-                || (!up
-                    && (map[new_x][new_y] >= (map[x][y] - 1))
-                    && (dist[new_x][new_y] > cost + 1))
+            if (dist[new_x][new_y] > cost + 1)
+                && ((up && (map[new_x][new_y] <= (map[x][y] + 1)))
+                    || (!up && (map[new_x][new_y] >= (map[x][y] - 1))))
             {
                 dist[new_x][new_y] = cost + 1;
                 heap.push(Reverse((cost + 1, new_x, new_y)));
@@ -48,17 +47,21 @@ fn main() {
         .collect();
     let _rows = map.len();
     let cols = map[0].len();
+
+    // prep map
     let (start_x, start_y) = find_pos(input, b'S', cols);
     let (end_x, end_y) = find_pos(input, b'E', cols);
     map[start_x][start_y] = b'a';
     map[end_x][end_y] = b'z';
 
+    // part 1
     let dist = dijkstra(&map, start_x, start_y, true);
     println!("{}", dist[end_x][end_y]);
 
     // part 2
     let (start_x, start_y) = (end_x, end_y);
     let dist = dijkstra(&map, start_x, start_y, false);
+
     let mut end_x = 0;
     let mut end_y = 0;
     let mut end = u32::MAX;
