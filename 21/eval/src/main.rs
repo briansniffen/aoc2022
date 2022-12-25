1dbeeba10451a387678a25ec22dbbe575d453976
Brian Sniffen <bsniffen@akamai.com>
2022-12-24 23:55:00 -0500

    day 21

--- 21/eval/src/main.rs
+++ 21/eval/src/main.rs
@@ -8,7 +8,7 @@ use nom::IResult;
 
 use std::collections::HashMap;
 
-#[derive(PartialEq,Eq,Debug,Copy,Clone)]
+#[derive(PartialEq, Eq, Debug, Copy, Clone)]
 enum Op {
     Add,
     Sub,
@@ -25,34 +25,19 @@ impl Op {
             Sub => a - b,
             Mul => a * b,
             Div => a / b,
-	    Eq => panic!(),
+            Eq => panic!(),
         };
         v
     }
 }
 
-#[derive(PartialEq,Eq,Debug)]
+#[derive(PartialEq, Eq, Debug)]
 enum Monkey {
     Val { v: i64 },
     Expr { a: String, op: Op, b: String },
     Unk,
 }
 use Monkey::*;
-
-impl Monkey {
-    fn val(&self) -> Option<i64> {
-	match self {
-	    Val{v} => Some(*v),
-	    _ => None
-	}
-    }
-    fn op(&self) -> Option<Op> {
-	match self {
-	    Expr{op,..} => Some(*op),
-	    _ => None
-	}
-    }
-}
 
 fn label(s: &str) -> IResult<&str, &str> {
     take(4usize)(s)
@@ -101,7 +86,7 @@ fn monkey_expr(s: &str) -> IResult<&str, Monkey> {
 }
 
 fn monkey_val(s: &str) -> IResult<&str, Monkey> {
-    let (rest, (v,_)) = tuple((i64,opt(newline)))(s)?;
+    let (rest, (v, _)) = tuple((i64, opt(newline)))(s)?;
     Ok((rest, Val { v }))
 }
 
@@ -119,77 +104,104 @@ fn monkeys(s: &str) -> IResult<&str, HashMap<String, Monkey>> {
     Ok((rest, env))
 }
 
-// n apply3(mut env: &HashMap<String,Monkey>, name: &str, left: &str, op: Op, right: &str) {
-//     if name=="humn" {return;};
-//     let mut change=None;
-//     let left = &env[left];
-//     let right = &env[right];
-//     if op==Eq {
-//     }
-//     match (left,right) {
-// 	(Val { v: a }, Val { v: b }) => {
-// 	    change=Some(name, op.apply(*a, *b));
-// 	},
-// 	(Expr {..}, Expr {..}) => change=None,
-// 	(Val {v: left}, Expr( a: right_a, op: right_op, b: right_b)) => {
-	    
-// 	}
-    
-    
-// }
-
-fn main() {
-    let input = include_str!("../input.txt");
-    let (rest, mut env) = monkeys(input).unwrap();
-    assert_eq!(rest, "");
-
-    // part 1
-    'part1: loop {
-	let labels = env.keys().cloned().collect::<Vec<_>>();
+fn simplify(env: &mut HashMap<String, Monkey>) {
+    let mut progress = false;
+    loop {
+        let labels = env.keys().cloned().collect::<Vec<_>>();
         for label in labels.into_iter() {
-	    let mut cell=None;
-            match &env[&label]{
-                Val { .. } => {
-                    if label == "root" {
-                        break 'part1;
-                    }
-                },
+            let mut cell = None;
+            match &env[&label] {
                 Expr { a, op, b } => {
                     cell = match (&env[a], &env[b]) {
                         (Val { v: a }, Val { v: b }) => Some(op.apply(*a, *b)),
                         _ => None,
                     }
-                },
-		Unk => panic!(),
+                }
+                _ => (),
             };
-	    match cell {
-		None => (),
-		Some(v) => *env.get_mut(&label).unwrap() = Val{v},
-	    }
+            match cell {
+                None => (),
+                Some(v) => {
+                    progress = true;
+                    *env.get_mut(&label).unwrap() = Val { v }
+                }
+            }
+        }
+        if !progress {
+            break;
+        } else {
+            progress = false;
+        };
+    }
+}
+
+//
+
+fn invert(op: &Op, known: &i64, val: &i64, flip: bool) -> i64 {
+    dbg!(op, known, val, flip);
+    match (op, flip) {
+        (Add, false) => val - known,
+        (Add, true) => val - known,
+        (Sub, false) => known - val,
+        (Sub, true) => val + known,
+        (Mul, false) => val / known,
+        (Mul, true) => val / known,
+        (Div, false) => known / val,
+        (Div, true) => known * val,
+        (Eq, _) => *known,
+    }
+}
+
+fn main() {
+    let input = include_str!("../input.txt");
+
+    //part 1
+    let (rest, mut env) = monkeys(input).unwrap();
+    assert_eq!(rest, "");
+    simplify(&mut env);
+    match env["root"] {
+        Val { v } => println!("part1: {}", v),
+        _ => panic!("error in part 1"),
+    }
+
+    //part 2
+    let (rest, mut env) = monkeys(input).unwrap();
+    assert_eq!(rest, "");
+    *env.get_mut("humn").unwrap() = Unk;
+    match env.get_mut("root").unwrap() {
+        Expr { ref mut op, .. } => *op = Eq,
+        _ => panic!(),
+    }
+    simplify(&mut env);
+    let mut point = "root";
+    let mut val = 0;
+    loop {
+        match &env[point] {
+            Unk => break,
+            Val { .. } => panic!(),
+            Expr { op, a, b } => {
+                match &env[a] {
+                    Val { v } =>
+                    // v is known, b is unknown
+                    {
+                        dbg!(a);
+                        val = invert(op, v, &val, false);
+                        point = b;
+                    }
+                    _ => (),
+                };
+                match &env[b] {
+                    Val { v } =>
+                    // v is known, a is unknown
+                    {
+                        dbg!(b);
+                        val = invert(op, v, &val, true);
+                        point = a;
+                    }
+                    _ => (),
+                }
+            }
         }
     }
-    match env["root"] {
-	Val{v} =>
-	    println!("part1: {}", v),
-	_ => panic!("error in part 1"),
-    }
-
-
-
-    // modify humn to Unk
-    // modify root to Eq
-    // 'part2: loop {
-    // 	let labels = env.keys().cloned().collect::<Vec<_>>();
-    //     for label in labels.into_iter() {
-	    
-
-
-    //     }
-    // }
-    // match env["humn"] {
-    // 	Val{v} =>
-    // 	    println!("part2: {}", v),
-    // 	_ => panic!("error in part 2"),
-    // }
-    
+    println!("part 2: {val}");
 }
